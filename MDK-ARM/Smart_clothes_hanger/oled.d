3c319ea7a829f272649e2a8f3005305ce3baa167
AbeRioW <261507455@qq.com>
2025-03-22 13:01:24 +0800

    oled init

---
+++ MDK-ARM/Smart_clothes_hanger/oled.d
@@ -0,0 +1,32 @@
+smart_clothes_hanger\oled.o: ..\BSP\oled.c
+smart_clothes_hanger\oled.o: ..\BSP\oled.h
+smart_clothes_hanger\oled.o: ../Core/Inc/gpio.h
+smart_clothes_hanger\oled.o: ../Core/Inc/main.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+smart_clothes_hanger\oled.o: ../Core/Inc/stm32f1xx_hal_conf.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+smart_clothes_hanger\oled.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+smart_clothes_hanger\oled.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h
+smart_clothes_hanger\oled.o: ../Drivers/CMSIS/Include/core_cm3.h
+smart_clothes_hanger\oled.o: C:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+smart_clothes_hanger\oled.o: ../Drivers/CMSIS/Include/cmsis_version.h
+smart_clothes_hanger\oled.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+smart_clothes_hanger\oled.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+smart_clothes_hanger\oled.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+smart_clothes_hanger\oled.o: C:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+smart_clothes_hanger\oled.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+smart_clothes_hanger\oled.o: C:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+smart_clothes_hanger\oled.o: ..\BSP\oledfont.h
+smart_clothes_hanger\oled.o: C:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
---
+++ MDK-ARM/Smart_clothes_hanger/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+smart_clothes_hanger\startup_stm32f103xb.o: startup_stm32f103xb.s
